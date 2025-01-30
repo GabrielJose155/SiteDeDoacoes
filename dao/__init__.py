86561c080045a4d6f01f874df47c7551085c68a9
Your Name <you@example.com>
2025-01-30 15:57:25 -0300

    first commit

---
+++ dao/__init__.py
@@ -0,0 +1,80 @@
+import psycopg2
+
+def conectardb():
+
+     con = psycopg2.connect(
+
+         host='localhost',
+         database='postgres',
+         user='postgres',
+         password='12345'
+     )
+     return con
+
+
+def login(email,senha):
+    con= conectardb()
+    cur = con.cursor()
+    sql = f"SELECT * from usuarios where email='{email}' and senha='{senha}'"
+    cur.execute(sql)
+    saida = cur.fetchall()
+    cur.close()
+    con.close()
+    return saida
+
+
+def inserir_user(nome,telefone,email,senha):
+
+    conn = conectardb()
+    cur = conn.cursor()
+    try:
+        sql = f"INSERT INTO usuarios (nome, telefone, email, senha) VALUES ('{nome}','{telefone}','{email}','{senha}')"
+        cur.execute(sql)
+
+    except psycopg2.IntegrityError:
+        #volta que deu ruim
+        conn.rollback()
+        exito = False
+    else:
+        conn.commit()
+        exito = True
+
+    cur.close()
+    conn.close()
+
+    return exito
+
+def inserir_roupa(tipo, tamanho, estado, descricao, email):
+    conn = conectardb()
+    cur = conn.cursor()
+
+    try:
+        sql = f"INSERT INTO cadastroderoupas (tipo, tamanho, estado, descricao, email) VALUES ('{tipo}','{tamanho}','{estado}','{descricao}', '{email}')"
+        cur.execute(sql)
+
+    except psycopg2.IntegrityError:
+        #volta que deu ruim
+        conn.rollback()
+        exito = False
+    else:
+        conn.commit()
+        exito = True
+
+    cur.close()
+    conn.close()
+
+    return exito
+
+
+
+def listar_roupas():
+    con = conectardb()
+    cur = con.cursor()
+    sql = f"SELECT  tipo, tamanho, estado, descricao  from cadastroderoupas"
+    cur.execute(sql)
+    saida = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return saida
---
+++ servidor.py
@@ -0,0 +1,94 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+
+import dao
+
+app = Flask(__name__)
+app.secret_key = 'gw4ab##@'
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'GET':
+        return  render_template('cadastro.html')
+    elif request.method == 'POST':
+        # Recebe os dados enviados pelo formulário
+        nome = request.form.get('nome')
+        telefone = request.form.get('telefone')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        dao.inserir_user(nome, telefone, email, senha)
+        session['longin'] = email
+        msg= 'Usuário inserido com sucesso!'
+        return render_template('login.html' ,texto=msg)
+    else:
+        tex = 'Erro ao inserir usuário.'
+        return render_template('cadastro.html', texto=tex)
+
+@app.route('/home')
+def index():
+     return render_template('principal.html')
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST','GET'])
+def ir_principal():
+    email_user = request.form.get('email')
+    senha_user = request.form.get('senha')
+    saida = dao.login(email_user, senha_user)
+
+    if len(saida) > 0:
+        session['login'] = email_user
+        nome_user = saida[0][0]
+        return render_template('principal.html', nome=nome_user)
+    else:
+        texto = 'O login que você fez não existe, tente novamente!'
+        return render_template('login.html', texto = texto )
+
+
+@app.route('/principal', methods= ['GET', 'POST'])
+def doar():
+    return render_template('Croupas.html')
+
+#botão quero doar
+@app.route('/pagcadastroroupas')
+def mostrar_pag_cadatro_roupas():
+    return render_template('Croupas.html')
+
+@app.route('/sair')
+def sair_pagCroupas():
+    return render_template('principal.html')
+
+@app.route('/protecao')
+def protecao():
+    if 'login' in session:
+        roupas = dao.inserir_roupa(session['email'])
+        return render_template('Croupas.html', lista=roupas)
+    else:
+        return render_template('login.html')
+
+@app.route('/Croupas', methods=['POST'])
+def doar_roupas():
+    tipo = request.form.get('tipo')
+    tamanho = request.form.get('tamanho')
+    estado = request.form.get('estado')
+    descricao = request.form.get('descricao')
+    email = session['login']
+
+    if dao.inserir_roupa(tipo, tamanho, estado, descricao, email):
+        return redirect(url_for('protecao'))
+    else:
+        return render_template('login.html')
+
+@app.route('/listar_roupas', methods=['GET','POST'])
+def listar_roupas():
+    roupas = dao.listar_roupas()
+    print(roupas)
+    return render_template('QueroReceber.html', lista=roupas)
+
+@app.route('/MinhaArea')
+def minha_area():
+    return render_template('MinhaArea.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
